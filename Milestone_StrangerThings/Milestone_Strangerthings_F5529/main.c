1bbf563fa155d548c1b1da9710b45fadb6fa248b
Jordan Alberico <albericoj3@students.rowan.edu>
2018-10-18 10:41:41 -0400

    Added Comments

--- Milestone_StrangerThings/Milestone_Strangerthings_F5529/main.c
+++ Milestone_StrangerThings/Milestone_Strangerthings_F5529/main.c
@@ -14,24 +14,15 @@ int ByteBuffer = 0;
 extern void LEDSetup();
 extern void TimerASetup();
 extern void resetColor();
+extern void UartSetup();
 
 int main(void)
 {
 
     WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
-
-    P4SEL |= BIT4+BIT5;                       // P3.3,4 = USCI_A1 TXD/RXD
-    UCA1CTL1 |= UCSWRST;                      // **Put state machine in reset**
-    UCA1CTL1 |= UCSSEL_2;                     // SMCLK
-    UCA1BR0 = 9;                              // 1MHz 115200 (see User's Guide)
-    UCA1BR1 = 0;                              // 1MHz 115200
-    UCA1MCTL |= UCBRS_1 + UCBRF_0;            // Modulation UCBRSx=1, UCBRFx=0
-    UCA1CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
-    UCA1IE |= UCRXIE;                         // Enable USCI_A1 RX interrupt
-
-    LEDSetup();
-    TimerASetup();
-
+    UartSetup();                              // Sets up Uart
+    LEDSetup();                               // Sets up LEDs
+    TimerASetup();                            // Sets up timers
     __bis_SR_register(LPM0_bits + GIE);       // Enter LPM0, interrupts enabled
     __no_operation();                         // For debugger
 }
@@ -56,6 +47,18 @@ void TimerASetup()
     TA0CCTL3 = OUTMOD_2; // Sets TACCR3 to toggle
 
     TA0CCR0 = 0xFF; // Sets TA0CCR0
+}
+
+void UartSetup()
+{
+    P4SEL |= BIT4+BIT5;                       // P4.4,5 = USCI_A1 TXD/RXD
+    UCA1CTL1 |= UCSWRST;                      // **Put state machine in reset**
+    UCA1CTL1 |= UCSSEL_2;                     // SMCLK
+    UCA1BR0 = 104;                            // 1MHz 9600 (see User's Guide)
+    UCA1BR1 = 0;                              // 1MHz 9600
+    UCA1MCTL |= UCBRS_1 + UCBRF_0;            // Modulation UCBRSx=1, UCBRFx=0
+    UCA1CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
+    UCA1IE |= UCRXIE;                         // Enable USCI_A1 RX interrupt
 }
 
 void resetColor()
@@ -84,34 +87,34 @@ void __attribute__ ((interrupt(USCI_A1_VECTOR))) USCI_A1_ISR (void)
         switch(count)
         {
         case 0:
-            UCA1TXBUF = UCA1RXBUF - 3;
-            ByteBuffer = UCA1RXBUF;
+            UCA1TXBUF = UCA1RXBUF - 3;          // Subtract 3 from the length byte and transmit
+            ByteBuffer = UCA1RXBUF;             // Store the number of expected  bytes
+            count++;                            // Determines what byte we are handling
+            break;
+        case 1:                                 //red LED
+            TA0CCR1 = UCA1RXBUF;                //sets red PWM
             count++;
             break;
-        case 1:
-            TA0CCR1 = UCA1RXBUF;
+        case 2:                                 //green LED
+            TA0CCR2 = UCA1RXBUF;                //sets green PWM
             count++;
             break;
-        case 2:
-            TA0CCR2 = UCA1RXBUF;
+        case 3 :                                //blue LED
+            TA0CCR3 = UCA1RXBUF;                //sets blue PWM
             count++;
             break;
-        case 3 :
-            TA0CCR3 = UCA1RXBUF;
-            count++;
-            break;
-        default:
-            if(count <= ByteBuffer)
+        default:                                //after the first four bytes
+            if(count <= ByteBuffer)             //if number of byes coming in is less than expected bytes,
             {
-                UCA1TXBUF = UCA1RXBUF;
+                UCA1TXBUF = UCA1RXBUF;          //pass on rest of the bytes
                 count++;
             }
-            else
+            else                                //handles next not expected byte as the first byte of a new package
             {
-                count = 1;
-                UCA1TXBUF = UCA1RXBUF - 3;
-                ByteBuffer = UCA1RXBUF;
-                resetColor();
+                count = 1;                      //reset count & skips case 0
+                UCA1TXBUF = UCA1RXBUF - 3;      //subtract 3 from the length byte
+                ByteBuffer = UCA1RXBUF;         //Store number of expected byte
+                resetColor();                   //resets LED PWM
             }
             break;
         }
